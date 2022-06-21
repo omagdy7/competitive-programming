42013225064413fdb31599d1b3304065c3964707
Omar Magdy <omar.professional8777@gmail.com>
2022-06-21 15:06:29 +0200

    Solved collegeLife2 from codechef

--- codechef/collegLife2/main.cpp
+++ codechef/collegLife2/main.cpp
@@ -8,9 +8,26 @@ int main () {
 	int tt;
 	cin >> tt;
 	while(tt--){
-    int s;
-    vector<int> q(s);
-
-
+    int s, introLength, numberOfEposides, eposideLength;
+    long long ans = 0;
+    cin >> s;
+    vector<int> seasons(s); // array containg the introLength of each season
+    for(auto &x : seasons) {
+      cin >> x;
+    }
+    for(int i = 0 ; i < s; i++) {
+      cin >> numberOfEposides;
+      for(int j = 0; j < numberOfEposides; j++) {
+        int x;
+        cin >> x;
+        if(j == 0) {
+          ans += x;
+        }
+        if(j > 0) {
+          ans += x - seasons[i];
+        }
+      }
+    }
+    cout << ans << endl;
 	}
 }
