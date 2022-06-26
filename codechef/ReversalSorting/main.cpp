13ae5b6eaaa4779009633eda567b4f1735669ac3
Omar Magdy <omar.professional8777@gmail.com>
2022-06-26 14:24:52 +0200

    partially solved reversal sorting on codechef

---
+++ codechef/ReversalSorting/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt;
+	cin >> tt;
+	while(tt--){
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for(int &i : v) cin >> i;
+    vector<vector<int>> sa;
+    // 1 2 2 3 2 2 3 3
+    for(int i = 0; i < v.size(); i++) {
+      for(int j = 0; j < v.size(); j++){
+
+      }
+    }
+	}
+}
